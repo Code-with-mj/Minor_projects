2c0d0ea60877c519ffa20a8ab12e0411b43b3860
Code-with-mj <uk01mj0001@gmail.com>
2023-09-27 17:08:38 +0530

    images

---
+++ Image_carousel/script.js
@@ -0,0 +1,24 @@
+const images = document.getElementById("images");
+const leftButton = document.getElementById("left");
+const rightButton = document.getElementById("right");
+
+const imagesList = document.querySelectorAll("#images img");
+let index = 0;
+
+const changeImage = () => {
+    if (index > imagesList.length - 1) index = 0;
+    else if (index < 0) index = imagesList.length - 1;
+    images.style.transform = `translateX(${-index * 500}px)`;
+};
+
+const run = () => {
+    index++;
+    changeImage();
+};
+
+const resetInterval = () => {
+    clearInterval(interval);
+    interval = setInterval(run, 2000);
+};
+
+let interval = setInterval(run, 2000);
